b7fdb50728f424f6f6b4af26c238cb3426abd2d4
Will Malisch <wmalisch@uwo.ca>
2020-10-29 15:07:13 -0400

    Edits to script1.sql

--- assignment2/script1.sql
+++ assignment2/script1.sql
@@ -16,8 +16,9 @@ show tables;
 
 /* Create tables */
 create table university(uniId int not null, officialName varchar(50) not null, city varchar(20) not null, provinceCode char(2) not null, nickname varchar(20) not null, primary key(uniId));
-create table outsideCourses(courseCode char(10) not null, courseName varchar(50), year int, weight float, uniId int not null, primary key(courseCode, uniId) foreign key(uniId) references university(uniId) on delete cascade);
 create table westernCourses(courseNumber char(6) not null, courseName varchar(50) not null, weight float not null, suffix varchar(3), primary key(courseNumber));
+show tables;
+create table outsideCourses(courseCode char(10) not null, courseName varchar(50), year int, weight float, uniId int not null, primary key(courseCode, uniId) foreign key(uniId) references university(uniId) on delete cascade);
 create table equivalence(courseNumber char(6) not null, courseCode char(10) not null, uniId int not null, dateDecided varchar(18) not null, primary key(courseNumber,courseCode,uniId) foreign key(courseNumber) references westernCourses(courseNumber) on delete cascade, foreign key(courseCode) references outsideCourses(courseCode) on delete cascade, foreign key(uniId) references university(uniId));
 
 /* Show tables again */
