a95c3b34342db3f89ce6d2c40601bf207eb7cb33
Will Malisch <wmalisch@uwo.ca>
2020-11-03 16:36:29 -0500

    php data request

---
+++ php/getdata.php
@@ -0,0 +1,15 @@
+
+<?php
+   $query = "SELECT * FROM owner";
+   $result = mysqli_query($connection,$query);
+   if (!$result) {
+        die("databases query failed.");
+    }
+   echo "Who are you looking up? </br>";
+   while ($row = mysqli_fetch_assoc($result)) {
+        echo '<input type="radio" name="petowners" value="';
+        echo $row["ownerid"];
+        echo '">' . $row["fname"] . " " . $row["lname"] . "<br>";
+   }
+   mysqli_free_result($result);
+?>
