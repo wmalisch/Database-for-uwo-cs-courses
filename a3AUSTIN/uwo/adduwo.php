a6e2f0efb208bc4c9eb35a06cbd603440ea616c7
wmalisch <malisch.will@gmail.com>
2020-11-20 14:35:39 -0500

    add course

--- a3AUSTIN/uwo/adduwo.php
+++ a3AUSTIN/uwo/adduwo.php
@@ -14,11 +14,11 @@
     $query = "INSERT INTO westernCourses VALUES('cs$number','$name','$weight','$suffix');"; 
     if(!mysqli_query($connection,$query)){
         echo "<h1>Add Error!</h1>";
-        die("Error while trying to update UWO course " . mysqli_error($connection));
         echo mysqli_error($connection);
         echo "<form action='adduwocourse.php method='post'>";
         echo "<input type='submit' value='Retry Entering Value'>";
         echo"</form>";
+        die("Error while trying to update UWO course " . mysqli_error($connection));
     }else{
         header('Location: ../index.php');
         exit;
