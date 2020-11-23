f84a4df5d65be929515f5989e6f3b87c71b3acb1
wmalisch <malisch.will@gmail.com>
2020-11-23 12:23:26 -0500

    delete courses

--- a3AUSTIN/uwo/delete/deleteuwo.php
+++ a3AUSTIN/uwo/delete/deleteuwo.php
@@ -12,19 +12,20 @@
     echo "<hr>";
     $number = $_POST["courseNumber"];
     $code = $_POST["courseCode"];
-    echo "<h5>Are you sure you want to delete the western course?". $number . "</h5>";
-
+    echo "<h5>Are you sure you want to delete the western course: </h5>";
+    echo "<h3>" . $number . "</h3>";
+    echo "<br>"
+    echo "<h5>If it has an equivalency, it will be listed below: </h5>";
+    echo "<h3>" . $code . "</h3>";
+    echo "<br>";
     echo "<form action='delete.php' method='post'>";
         echo "<button type='submit' name='courseNumber' value='" . $number ."'>Yes I am sure</button>";
     echo"</form>";
+    echo "<br>";
     echo "<form action='deleteuwocourse.php' method='post'>";
         echo "<button type='submit' value='" . $number ."'>No, do not delete</button>";
     echo"</form>";
     echo "<br>";
-    echo "<h5>If it has an equivalency, it will be listed below: </h5>";
-    echo "<h5>" . $code . "</h5>";
-    
-    
 ?>
 </body>
 </html>
