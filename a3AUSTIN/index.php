865d9c3241781308b9963e4c5926079abefa1130
wmalisch <malisch.will@gmail.com>
2020-11-24 12:50:39 -0500

    see quivalents

--- a3AUSTIN/index.php
+++ a3AUSTIN/index.php
@@ -45,8 +45,18 @@
                 <label>Month: </label>
                 <select name='month'>
                     <?php
-                        $i = 0;
-                        while($i != 12){
+                        $i = 1;
+                        while($i != 13){
+                            echo "<option value='" . $i . "'>" . $i . "</option>";
+                            $i = $i + 1;
+                        }
+                    ?>
+                </select>
+                <label>Day: </label>
+                <select name='day'>
+                    <?php
+                        $i = 1;
+                        while($i != 32){
                             echo "<option value='" . $i . "'>" . $i . "</option>";
                             $i = $i + 1;
                         }
